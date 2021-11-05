f360cea46b0987ed28b62db1942ee305d842f158
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-11-05 15:55:37 +0100

    :lipstick: add style to header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,13 +1,36 @@
 <template>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="#landing">S</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#portfolio">Portfolio</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
+        <nav class="nav">
+            <a class="logo" href="#landing">S</a>
+            <div class="links-container">
+                <a class="nav-link" href="#about">About</a>
+                <a class="nav-link" href="#portfolio">Portfolio</a>
+                <a class="nav-link" href="#contact">Contact</a>  
+            </div>
         </nav>
-    </header>
 </template>
+
+<style scoped lang="scss">
+.nav {
+    display: flex;
+    padding: 0 12rem 0 12rem;
+
+    .logo {
+        font-size: 6rem;
+        font-family: $font-primary;
+    }
+    .links-container {
+        align-items: center;
+        flex: 1;
+        display: flex;
+        justify-content: center;
+
+        .nav-link { 
+            font-family: $font-secondary;
+            font-size: 1.7rem;
+            padding: 0 5rem 0 5rem;
+        }
+    }
+}
+
+</style>
 
