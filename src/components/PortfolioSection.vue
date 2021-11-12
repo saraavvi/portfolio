5a9135799341bef0e951491aefe8f114fc5545d8
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-11-12 14:11:00 +0100

    :sparkles: create porfolio section and project-cards

--- src/components/PortfolioSection.vue
+++ src/components/PortfolioSection.vue
@@ -1,5 +1,46 @@
 <template>
-    <section id="portfolio">
-        <h3> Portfolio section </h3>
-    </section>
+  <section id="portfolio">
+    <ProjectCard
+      class="project-card"
+      v-for="project in projects"
+      :key="project"
+      :name="project.name"
+      :image="project.image"
+      :description="project.description"
+    />
+  </section>
 </template>
+
+<script>
+import ProjectCard from "./ProjectCard";
+export default {
+  data() {
+    return {
+      projects: {
+        weatherForecast: {
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores a accusantium perspiciatis harum quisquam possimus tempora libero vel eius ullam, nostrum repellat eveniet optio corrupti labore. Vel soluta odio esse!",
+          image: "weather-forecast",
+          name: "Weather Forecast",
+        },
+        manCave: {
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores a accusantium perspiciatis harum quisquam possimus tempora libero vel eius ullam, nostrum repellat eveniet optio corrupti labore. Vel soluta odio esse!",
+          image: "mancave",
+          name: "Mancave",
+        },
+      },
+    };
+  },
+  components: {
+    ProjectCard,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.project-card {
+  margin-bottom: 10rem;
+  box-shadow: $box-shadow;
+}
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="project-container">
+    <div class="image-container">
+      <img class="image" :src="'/images/' + image + '.png'" />
+    </div>
+    <div class="description-container">
+      <h2 class="heading">{{ name }}</h2>
+      <p class="description">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.project-container {
+  border-radius: 2rem;
+  height: 400px;
+  display: flex;
+
+  @include hover-lift;
+
+  .image-container {
+    top: 3rem;
+    height: 100%;
+    padding: 3rem;
+
+    .image {
+      border-radius: 2rem;
+      object-fit: contain;
+      box-shadow: $box-shadow;
+      height: 100%;
+    }
+  }
+  .description-container {
+    padding: 3rem;
+
+    .heading {
+      font-family: $font-secondary;
+      font-size: 5rem;
+      margin-bottom: 2rem;
+    }
+  }
+}
+</style>
