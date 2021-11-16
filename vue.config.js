1c89ebe0e49086eeb163b41d4017f9cd0ef21882
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-11-16 14:01:50 +0100

    :recycle: split sass variables and mixins to separate files

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-    css: {
-        loaderOptions: {
-            sass: {
-                additionalData: `@import "@/assets/_shared.scss";`
-            }
-        }
-    }
-}
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+        @import "@/assets/_variables.scss";
+        @import "@/assets/_mixins.scss";
+        `,
+      },
+    },
+  },
+};
