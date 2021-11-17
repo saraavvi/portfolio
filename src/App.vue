1b7f848fda93598b06b8301a0a1d920ffecdf77b
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-11-17 09:33:45 +0100

    :fire: remove footer

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <TheHeader />
   <ThePortfolioPage />
-  <TheFooter />
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader";
-import TheFooter from "./components/TheFooter";
 import ThePortfolioPage from "./pages/PortfolioPage";
 export default {
   name: "App",
   components: {
     TheHeader,
     ThePortfolioPage,
-    TheFooter,
   },
 };
 </script>
