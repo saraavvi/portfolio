8b80a1b946465810953df6f34eeb61754273701a
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-11-18 13:35:08 +0100

    :lipstick: add logo

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,9 @@
 <template>
   <nav class="nav">
-    <a class="logo" href="#landing">S</a>
     <div class="links-container">
+      <a class="logo" href="#landing"
+        ><img class="logo" src="/images/logo2.png"
+      /></a>
       <a class="nav-link" href="#about">About</a>
       <a class="nav-link" href="#portfolio">Portfolio</a>
       <a class="nav-link" href="#contact">Contact</a>
@@ -11,42 +13,40 @@
 
 <style scoped lang="scss">
 .nav {
-  display: flex;
-  padding: 0rem 16rem;
+  padding: 2rem;
+  margin: 0 auto;
   position: sticky;
   top: 0;
   background-color: $color-background;
   z-index: 1000;
 
-  @include respond(tab-port) {
-    padding: 0 8rem 0 8rem;
-  }
   @include respond(phone) {
     padding: 0;
   }
 
-  .logo {
-    font-size: 5rem;
-    font-family: $font-secondary;
-
-    @include respond(phone) {
-      display: none;
-    }
-  }
   .links-container {
+    width: 100%;
     align-items: center;
-    flex: 1;
     display: flex;
     justify-content: center;
-  }
-  .nav-link {
-    @include hover-underline($color-secondary);
-    font-size: 1.7rem;
-    padding: 1rem 5rem;
-    font-weight: bold;
 
-    @include respond(phone) {
-      padding: 2rem;
+    .logo {
+      height: 5rem;
+      margin-right: auto;
+
+      @include respond(phone) {
+        display: none;
+      }
+    }
+    .nav-link {
+      @include hover-underline($color-secondary);
+      font-size: 1.7rem;
+      padding: 1rem 5rem;
+      font-weight: bold;
+
+      @include respond(phone) {
+        padding: 2rem;
+      }
     }
   }
 }
